1b14e7207f58073b2ea4f721885a149e735875fa
SignalMedic <dispensable.medic@protonmail.com>
2024-03-11 08:21:30 -0400

    Change debug settings

--- WigletoTAK.py
+++ WigletoTAK.py
@@ -206,4 +206,4 @@ def create_cot_xml_payload_point(mac, ssid, firstseen, channel, rssi, currentlat
     </event>'''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
